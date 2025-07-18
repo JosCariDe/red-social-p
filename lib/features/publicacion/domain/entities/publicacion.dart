5c987b8e0fefbf185e84278291ae3d9536b7255e
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-18 17:36:40 -0500

    feat/Entidad de Pokemon creado

--- lib/features/publicacion/domain/entities/publicacion.dart
+++ lib/features/publicacion/domain/entities/publicacion.dart
@@ -1,3 +1,31 @@
-class Publicacion {
-  final
+class Post {
+  int id;
+  String title;
+  String body;
+  List<String> tags;
+  Reactions reactions;
+  int views;
+  int userId;
+
+  Post({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.tags,
+    required this.reactions,
+    required this.views,
+    required this.userId,
+  });
+
+}
+
+class Reactions {
+  int likes;
+  int dislikes;
+
+  Reactions({
+    required this.likes,
+    required this.dislikes,
+  });
+
 }
