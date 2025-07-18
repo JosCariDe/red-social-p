f3ec721eaa7be189eeceac6ec856e56a8b3bbbf2
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-18 17:25:55 -0500

    firts Commit

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,7 @@
+
+abstract class Failure {}
+
+class ServerFailure extends Failure{}
+
+
+class LocalFailure extends Failure{}
---
+++ lib/core/utils/utils.dart
@@ -0,0 +1,5 @@
+
+import 'dart:math';
+
+int get randomPokemonId => Random().nextInt(100) + 1;
+
