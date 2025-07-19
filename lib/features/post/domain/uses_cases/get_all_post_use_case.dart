77af0bfa44aa70915355591c2d477847fefdccbc
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-18 22:36:38 -0500

    feat/nombres de caso de usos actualizado Primer caso de uso creado, va a llamar todos los posts

---
+++ lib/features/post/domain/uses_cases/get_all_post_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:red_social_prueba/core/errors/failure.dart';
+import 'package:red_social_prueba/features/post/domain/repositories/post_repository.dart';
+import 'package:red_social_prueba/shared/domain/entities/post.dart';
+
+class GetAllPostUseCase {
+  final PostRepository postRepository;
+
+  GetAllPostUseCase({required this.postRepository});
+
+  Future<Either<Failure, List<Post>>> call() {
+    return postRepository.getAllPost();
+  }
+}
