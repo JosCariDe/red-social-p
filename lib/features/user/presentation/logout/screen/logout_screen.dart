07d765ff66276d8d5ce08fe027141fe3983b0c0c
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-20 17:53:52 -0500

    feat/Btn Mejorados

--- lib/features/user/presentation/logout/screen/logout_screen.dart
+++ lib/features/user/presentation/logout/screen/logout_screen.dart
@@ -5,6 +5,7 @@ import 'package:red_social_prueba/features/post/presentation/home/blocs/get_all_
 import 'package:red_social_prueba/features/post/presentation/home/blocs/get_all_posts/get_all_posts_event.dart';
 import 'package:red_social_prueba/features/post/presentation/posts_user/blocs/user_posts_bloc/user_posts_bloc.dart';
 import 'package:red_social_prueba/features/user/presentation/logout/blocs/logout_bloc/logout_bloc.dart';
+import 'package:red_social_prueba/shared/widgets/action_button_widget.dart';
 
 class LogoutScreen extends StatelessWidget {
   const LogoutScreen({super.key});
@@ -20,17 +21,37 @@ class LogoutScreen extends StatelessWidget {
               context.read<GetAllPostsBloc>().add(ResetPosts());
               context.read<UserPostsBloc>().add(ResetUserPosts());
               // Navegar a la pantalla de login o inicial
-              context.push('/login');
+              GoRouter.of(context).go('/login');
             }
           },
           builder: (context, state) {
             if (state is LogoutLoading) {
               return CircularProgressIndicator();
             }
-            return ElevatedButton(
-              child: Text('Cerrar sesión'),
-              onPressed: () {
-                context.read<LogoutBloc>().add(LogoutRequested());
+            return BlocBuilder<LogoutBloc, LogoutState>(
+              builder: (context, state) {
+                if (state is LogoutLoading) {
+                  return const CircularProgressIndicator();
+                }
+                return Center(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 500  ,),
+                      ActionButtonWidget(
+                        onPressed: () {
+                          context.read<LogoutBloc>().add(LogoutRequested());
+                        },
+                        primaryText: 'Cerrar sesión',
+                        icon: Icons.logout_rounded,
+                        primaryColor: Theme.of(context).colorScheme.primary,
+                        isLoading: state is LogoutLoading,
+                        loadingText: 'Cerrando sesión...',
+                        showCancelButton: false,
+                        height: 45,
+                      ),
+                    ],
+                  ),
+                );  
               },
             );
           },
